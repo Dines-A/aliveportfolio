93b5ba4ddceebf00ca1a81c613c486b5c6ce5abe
Dinesh <dinesha.ei19@bitsathy.ac.in>
2022-02-14 01:16:53 +0530

    my alive portfolio

---
+++ Email.js
@@ -0,0 +1,25 @@
+let loader = document.getElementById("preloader");
+window.addEventListener("load",
+  function ()
+  {
+    loader.style.display = "none";
+  })
+
+
+
+// function sendEmail()
+// {
+//   Email.send({    
+//     SecureToken :"ee71c4aa-6712-4f18-9c87-266bd9867583",
+//     To : 'dinesha.ei19@bitsathy.ac.in',
+//     From : document.getElementById("email").value,
+//     Subject : "You got mail :-",
+//     Body: "Name :" + document.getElementById("name").value
+//                            +"<br> Email :"+ document.getElementById("email").value
+//                            +"<br> Phone no :"+ document.getElementById("phone").value
+//                            +"<br> Message :"+ document.getElementById("msg").value
+// }).then(
+//   message => alert("Message Sent Succesfully")
+// );
+// }
+
